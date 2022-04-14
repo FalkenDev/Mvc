f287450c8d9b32245527883d75ef83f392173118
“FalkenDev” <kafa21@student.bth.se>
2022-04-14 22:17:59 +0100

    Added draw and draw/:number route

--- src/Controller/CardControllerTwig.php
+++ src/Controller/CardControllerTwig.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -23,7 +25,6 @@ class CardControllerTwig extends AbstractController
     public function deck(): Response
     {
         $die = new \App\Card\Deck();
-        $die->decks();
         $die = $die->show_deck();
         $data = [
             'title' => "Deck of cards",
@@ -33,20 +34,79 @@ class CardControllerTwig extends AbstractController
     }
 
     /**
-     * @Route("/card/deck/draw", name="draw")
+     * @Route("/card/deck/draw", name="draw", methods={"GET","HEAD"})
      */
-    public function draw(): Response
-    {
-        return $this->render('card/draw.html.twig');
+    public function draw(
+        Request $request,
+        SessionInterface $session
+    ): Response {
+        $numDraw = 1;
+        $die = $session->get("deck") ?? new \App\Card\Deck();
+        if (count($die->show_deck()) < 1)
+        {
+            $data = [
+                'title' => "Deck of cards",
+                'deck' => $session->get("draw"),
+                'draws' => count($die->show_deck())
+            ];
+            return $this->render('card/draw.html.twig', $data);
+        }
+
+        $draw = $die->draw($numDraw);
+        $session->set("deck", $die);
+        $session->set("draw", $draw);
+
+        $data = [
+            'title' => "Deck of cards",
+            'deck' => $draw,
+            'draws' => count($die->show_deck())
+        ];
+        return $this->render('card/draw.html.twig', $data);
     }
+
+    /**
+     * @Route("/card/deck/draw/{numDraw}", name="draws", methods={"GET","HEAD"})
+     */
+    public function drawSelected(
+        int $numDraw = 1,
+        Request $request,
+        SessionInterface $session
+    ): Response {
+        $die = $session->get("deck") ?? new \App\Card\Deck();
+        if (count($die->show_deck()) < $numDraw )
+        {
+            $data = [
+                'title' => "Deck of cards",
+                'deck' => $session->get("draw"),
+                'draws' => count($die->show_deck())
+            ];
+            return $this->render('card/draw.html.twig', $data);
+        }
+
+        $draw = $die->draw($numDraw);
+        $session->set("deck", $die);
+        $session->set("draw", $draw);
+
+        $data = [
+            'title' => "Deck of cards",
+            'deck' => $draw,
+            'draws' => count($die->show_deck())
+        ];
+        return $this->render('card/draw.html.twig', $data);
+    }
+
+    
+
 
     /**
      * @Route("/card/deck/shuffle", name="shuffle")
      */
-    public function shuffle(): Response
-    {
-        $die = new \App\Card\Deck();
-        $die->decks();
+    public function shuffle(
+        Request $request,
+        SessionInterface $session
+    ): Response {
+        $session->clear();
+        $die = $session->get("deck") ?? new \App\Card\Deck();
         $die = $die->shuffle_deck();
         $data = [
             'title' => "Deck of cards",
