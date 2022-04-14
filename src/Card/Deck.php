207603b3dee1c4ac11e895833644abbb6b7795da
“FalkenDev” <kafa21@student.bth.se>
2022-04-14 22:17:05 +0100

    Added Draw function and find a way to use __construct function to create deck

--- src/Card/Deck.php
+++ src/Card/Deck.php
@@ -5,13 +5,14 @@ namespace App\Card;
 // Deck Class
 class Deck
 {
-    private array $deck = [];
+    private array $deck;
     private array $values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
     private array $suits = ["♠", "♥", "♣", "♦"];
 
     // Buiding the deck from the variables insert into the deck
-    public function decks()
+    public function __construct(array $deck = [])
     {
+        $this->deck = $deck;
         foreach ($this->suits as $suit) {
 
             foreach ($this->values as $value) {
@@ -30,5 +31,21 @@ class Deck
     {
         shuffle($this->deck);
         return $this->deck;
+    }
+
+    public function draw(int $numDraw)
+    {
+        $random_card = array_rand($this->deck, $numDraw);
+        for ($i = 1; $i <= $numDraw; $i++)
+        {
+            if (is_array($random_card)) {
+                $card[] = $this->deck[$random_card[$i - 1]];
+                unset($this->deck[$random_card[$i - 1]]);
+            } else {
+                $card[] = $this->deck[$random_card];
+                unset($this->deck[$random_card]);
+            }
+        }
+        return $card;
     }
 }
