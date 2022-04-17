b41a88978b177d57c5bf5ec16171e32eee7c6fd3
“FalkenDev” <kafa21@student.bth.se>
2022-04-17 21:02:50 +0100

    Added tool phpstan

---
+++ tools/phpstan/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c23721d469da6f4de5be89a68ae64f6a75acc868',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c23721d469da6f4de5be89a68ae64f6a75acc868',
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '1.5.6',
+            'version' => '1.5.6.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'reference' => '799dd8c2d2c9c704bb55d2078078cb970cf0f6d1',
+            'dev_requirement' => false,
+        ),
+    ),
+);
