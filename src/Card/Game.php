ae3c77ef4f3c5c670a043c192340928ca661960c
“FalkenDev” <kafa21@student.bth.se>
2022-04-19 01:49:25 +0100

    Added Blackjack class

---
+++ src/Card/Game.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Card;
+
+// Deck Class
+class Blackjack
+{
+    private array $deck;
+    private array $dealerHand;
+    private array $playerHand;
+
+    // Buiding the deck from the variables insert into the deck
+    public function __construct()
+    {
+
+    }
+}
